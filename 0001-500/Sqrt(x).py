f7f9f1960a30913939d01511971494ab9f2c72bb
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2024-03-27 08:38:50 +0530

    69

---
+++ 0001-500/Sqrt(x).py
@@ -0,0 +1,6 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        square_root=sqrt(x)
+        float_integer=int(square_root)
+        print(float_integer)
+        return float_integer
