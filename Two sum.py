dbdff47221aca110d1c7e5730cb6a9c7d3460f48
imDarshanGK <darshan241210s@gmail.com>
2024-11-03 23:22:33 +0530

    884

--- Two sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        dict={}
-        for i,n in enumerate(nums):
-            if n in dict:
-                return dict[n],i
-            else:
-                dict[target-n]=i
